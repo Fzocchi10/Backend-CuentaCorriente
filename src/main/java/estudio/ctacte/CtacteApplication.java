e7f9e0310548b547889707fce8e2c724dcefc3ba
Fzocchi10 <fzocchi97@gmail.com>
2025-11-12 14:15:22 -0300

    Commit Inicial

---
+++ src/main/java/estudio/ctacte/CtacteApplication.java
@@ -0,0 +1,13 @@
+package estudio.ctacte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CtacteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CtacteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/estudio/ctacte/CtacteApplicationTests.java
@@ -0,0 +1,13 @@
+package estudio.ctacte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CtacteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
